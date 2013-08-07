a64820a64da8164696ed5d3dca5a47f5a8da5ab0
Steve Streza <steve@stevestreza.com>
2013-08-07 14:05:03 -0700

    Moved search code into the template. No need for a separate file there.

--- public/javascripts/search.js
+++
@@ -1,9 +0,0 @@
-var $rows = $("#services").children("div");
-console.log($rows);
-$('#search').keyup(function() {
-    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-    $rows.show().filter(function() {
-        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-        return !~text.indexOf(val);
-    }).hide();
-});
